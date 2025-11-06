1165469b7d4d8f0e6a5ef49dda5a3b57cd112a7b
Lukman Hakim <luky.kun@gmail.com>
2025-11-06 15:59:32 +0700

    Initial project

---
+++ mypokemon/mypokemon/LoginView.swift
@@ -0,0 +1,26 @@
+//
+//  LoginView.swift
+//  mypokemon
+//
+//  Created by Lukman Hakim on 06/11/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ mypokemon/mypokemon/mypokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  mypokemonApp.swift
+//  mypokemon
+//
+//  Created by Lukman Hakim on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct mypokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
